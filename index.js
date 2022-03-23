4da4235e1e6cef01c14f7bb5302de705dcc3b0da
Siavash Adib <Siyavash.Adib@gmail,com>
2022-03-23 19:59:56 +0430

    Game is up and running

--- index.js
+++ index.js
@@ -1,24 +1,160 @@
-console.log("Whats Up ?");
+let playIcons = [...document.querySelectorAll('.play-icon')];
+let versusIcon = document.querySelector('.vs-icon');
+let gamePanel = document.querySelector('.game-panel');
+let scorePanel = document.querySelector('.score');
+
 
 const PLAYS = [
   {
-    play: "Rock",
-    wins: "Scissors",
-    loosesTo: "Paper"
+    play: "rock",
+    wins: "scissor",
+    loosesTo: "paper"
   },
   {
-    play: "Scissors",
-    wins: "Paper",
-    loosesTo: "Rock"
+    play: "scissor",
+    wins: "paper",
+    loosesTo: "rock"
   },
   {
-    play: "Paper",
-    wins: "Rock",
-    loosesTo: "Scissors"
+    play: "paper",
+    wins: "rock",
+    loosesTo: "scissor"
   }
 ];
 
-function computerPlay() {
+
+let gameState = 'waiting for user play';
+let userPlay = '';
+let botPlay = '';
+let botPlayIcon = '';
+let playerScore =  0;
+let computerScore =  0;
+
+
+playIcons.forEach((playIcon) => playIcon.addEventListener(
+  'click', gameStateMachine)
+);
+
+function gameRestart() {
+  gameState = 'none';
+  
+  userPlay.removeEventListener('animationend', gameStateMachine);
+  userPlay.classList.remove('shaking');
+
+  botPlayIcon.classList.add('inactive');
+  versusIcon.classList.add('inactive');
+  userPlay.classList.add('inactive');
+  gamePanel.style.setProperty('--result-font', '0');
+  
+  botPlayIcon.addEventListener('transitionend', (event) => {
+    if(event.propertyName !== 'transform') {
+      return;
+    }
+
+    playIcons.forEach((playIcon) => {
+      playIcon.classList.remove('inactive');
+      playIcon.removeEventListener('transitionend', gameStateMachine);
+    });
+  
+    gamePanel.removeChild(botPlayIcon);
+    
+    [...document.querySelectorAll('[class*="-icon"]')].forEach((node) => {
+      node.style.filter = 'none';
+    });
+
+    gameState = 'waiting for user play';
+  });
+}
+
+function gameStateMachine(event=null) {
+  // console.log(event);
+  // console.log('State Machine in State :', gameState)
+  switch(gameState) {
+    case 'waiting for user play':
+      // console.log(this.id);
+      userPlay = this;
+
+      this.classList.add('shaking');
+      this.addEventListener('animationend', gameStateMachine);
+
+      gameState = 'waiting for shaking animation';
+      break;
+    
+    case 'waiting for shaking animation':
+      this.classList.remove('shaking');
+
+      let firstObject = true;
+      playIcons.forEach((playIcon) => {
+        if(playIcon.id != userPlay.id) {
+          playIcon.classList.add('inactive');
+          
+          if(firstObject) {
+            firstObject = false;
+            playIcon.addEventListener('transitionend', gameStateMachine);
+          }
+        }
+      });
+
+      botPlay = computerGeneratePlay();
+
+      botPlayIcon = playIcons[
+        playIcons.findIndex((icon => icon.id === botPlay))
+      ].cloneNode(true);
+      botPlayIcon.id = 'botplay-icon';
+      // console.log(botPlayIcon);
+      botPlayIcon.classList.add('inactive');
+      gamePanel.appendChild(botPlayIcon);
+    
+      gameState = 'waiting for fading animation';
+      break;
+
+    case 'waiting for fading animation':
+      if(event.propertyName !== 'height') {
+        return;
+      }
+
+      window.setTimeout(() => {
+        versusIcon.classList.remove('inactive');
+        botPlayIcon.classList.remove('inactive');
+        botPlayIcon.addEventListener('transitionend', gameStateMachine);
+      }, 200)
+      
+      gameState = 'waiting for bot play icon animation';
+      break;
+
+    case 'waiting for bot play icon animation':
+      if(event.propertyName !== 'transform' || this.id !== 'botplay-icon') {
+        return;
+      }
+
+      window.setTimeout(() => {
+        [...document.querySelectorAll('[class*="-icon"]')].forEach((node) => {
+          // node.classList.add('inactive');
+          node.style.filter = 'blur(5px)';
+        });
+        
+        let roundResult = playRound(userPlay.id, botPlay);
+        console.log(roundResult);
+        
+        if(roundResult.includes("Player"))   {
+          playerScore++;
+          gamePanel.style.setProperty('--result-content', '"YOU WIN"');
+        } else if(roundResult.includes("Computer")) {
+          computerScore++;
+          gamePanel.style.setProperty('--result-content', '"YOU LOOSE"');
+        } else {
+          gamePanel.style.setProperty('--result-content', '"TIE"');
+        }
+        gamePanel.style.setProperty('--result-font', '15vw');
+        scorePanel.innerHTML = `<pre>Score :   ${playerScore}  vs  ${computerScore}</pre>`;
+
+        window.setTimeout(gameRestart, 1000)
+      }, 200);
+      break;
+  }
+}
+
+function computerGeneratePlay() {
   return PLAYS[
     Math.floor(Math.random() * PLAYS.length)].play;
 }
@@ -39,23 +175,22 @@ function playRound(playerSelection, computerSelection) {
   return result;
 }
 
-let playerScore =  0;
-let computerScore =  0;
 
-for(let i = 0; i < 5; i++) {
-  let userInput =  window.prompt();
-  let roundResult = playRound(userInput, computerPlay());
+// for(let i = 0; i < 5; i++) {
+//   let userInput =  window.prompt();
+//   let roundResult = playRound(userInput, computerPlay());
   
-  console.log(`round ${i}`, roundResult);
-  if(roundResult.includes("Tie")) { continue; }
-  if(roundResult.includes("Player"))   { playerScore++; continue; }
-  if(roundResult.includes("Computer")) { computerScore++; continue; }
+//   console.log(`round ${i}`, roundResult);
+//   if(roundResult.includes("Tie")) { continue; }
+//   if(roundResult.includes("Player"))   { playerScore++; continue; }
+//   if(roundResult.includes("Computer")) { computerScore++; continue; }
   
-  i--;
-}
+//   i--;
+// }
 
-console.log(
-  "Final Result:\n",
-  "playerScore =", playerScore,
-  "computerScore =", computerScore
-)
+// console.log(
+//   "Final Result:\n",
+//   "playerScore =", playerScore,
+//   "computerScore =", computerScore
+// )
+
